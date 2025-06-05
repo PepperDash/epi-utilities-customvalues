de87f9a5877a5e9b36e9bdf1543ba1e20f95ed68
Jason DeVito <jdevito@pepperdash.com>
2025-06-05 10:52:16 -0500

    fix: improve logging and error handling in CustomValuesController

--- src/CustomValuesController.cs
+++ src/CustomValuesController.cs
@@ -8,6 +8,7 @@ using Essentials.Plugin.CustomValues;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using PepperDash.Core;
+using PepperDash.Core.Logging;
 using PepperDash.Essentials.Core;
 using PepperDash.Essentials.Core.Bridges;
 using PepperDash.Essentials.Core.Config;
@@ -31,7 +32,7 @@ namespace PDT.CustomValues.EPI
             {
                 if (string.IsNullOrEmpty(props.FilePath))
                 {
-                    Debug.Console(0, this, "File path is not specified... so we are going to save these values locally");
+                    Debug.Console(0, this, "File path not specified... saving values locally");
                     return;
                 }
 
@@ -46,26 +47,35 @@ namespace PDT.CustomValues.EPI
                 }
                 catch (Exception ex)
                 {
-                    Debug.Console(0, this, Debug.ErrorLogLevel.Warning, "Failed to save custom values data:{0}", ex);
+                    Debug.Console(0, this, Debug.ErrorLogLevel.Warning, "Failed to save custom values data: '{0}'", ex);
                 }
             }),
                 Timeout.Infinite);
 
             props = config.Properties.ToObject<CustomValuesConfigObject>();
 
-            AddPreActivationAction(() => data = 
-                string.IsNullOrEmpty(props.FilePath) 
+            data = string.IsNullOrEmpty(props.FilePath)
                 ? props.Data
-                : SeedData(props.FilePath, props.Seed));
+                : SeedData(props.FilePath, props.Seed);
+
+            if (data == null)
+            {
+                Debug.Console(0, this, "No data found in the config or file, using an empty JObject.");
+                data = new JObject();
+            }
+            else
+            {
+                Debug.Console(1, this, "Loaded data from file: {0}", props.FilePath);
+            }
         }
 
         public void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
         {
             if (data == null)
-                throw new Exception("The seed data is null... something went wrong.  Check that you have either defined a file path or a 'data' object in the config");
+                throw new NullReferenceException("LinkToApi: data is null.  Check file path or a 'data' object is defined in the config");
 
             var feedbacks = new List<Feedback>();
-            DataSaved += (sender, args) => feedbacks.ToList().ForEach(f => f.FireUpdate());
+            DataSaved += (sender, args) => feedbacks.ForEach(f => f.FireUpdate());
 
             var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
             if (customJoins == null)
@@ -81,98 +91,113 @@ namespace PDT.CustomValues.EPI
             {
                 var path = customJoin.Key;
                 var index = customJoin.Value.JoinNumber;
-                var join = (ushort) (index + joinStart - 1);
-
-                Debug.Console(1, this, "Attempting to map join:{0} to path:{1}", join, path);
-                var value = data.SelectToken(path);
+                var join = (ushort)(index + joinStart - 1);
 
                 // Validity Checks
                 if (index == 0)
                 {
-                    Debug.Console(0, this, "Missing Join number for path:{0}", path);
+                    Debug.Console(0, this, "Cannot map path: '{0}', missing join number", path);
                     continue;
                 }
 
-                Debug.Console(1, this, "Mapping join:{0} to value:{1} with path:{2}", join, value.ToString(), path);
+                Debug.Console(1, this, "Attempting to map path: '{1}' to join: '{0}'", join, path);
+                var value = data.SelectToken(path);
+                if (value == null)
+                {
+                    Debug.Console(0, this, "No value found for path: '{0}' in '{1}', ignoring path, update customValues if needed.", path, props.FilePath);
+                    continue;
+                }
+
+                Debug.Console(1, this, "Mapping path: '{1}' to join: '{0}' as type: '{2}' with value: '{3}'",
+                    join, path, value.Type.ToString(), value);
 
                 switch (value.Type)
                 {
                     case JTokenType.Integer:
-                    {
-                        Debug.Console(2, this, "I AM INT");
-
-                        trilist.SetUShortSigAction(@join, x => WithLock(() =>
                         {
-                            data.SelectToken(path).Replace(x);
-                            saveTimer.Reset(1000);
-                        }));
+                            //Debug.Console(2, this, "I AM INT");
 
-                        var feedback = new IntFeedback(() => data.SelectToken(path).Value<int>());
-                        feedback.LinkInputSig(trilist.UShortInput[@join]);
-                        feedbacks.Add(feedback);
-                        feedback.FireUpdate();
-                        feedback.OutputChange +=
-                            (sender, args) =>
-                                Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.IntValue);
-                    }
-                        break;
+                            trilist.SetUShortSigAction(@join, x => WithLock(() =>
+                            {
+                                data.SelectToken(path).Replace(x);
+                                saveTimer.Reset(1000);
+                            }));
+
+                            var feedback = new IntFeedback(() => data.SelectToken(path).Value<int>());
+                            feedback.LinkInputSig(trilist.UShortInput[@join]);
+                            feedbacks.Add(feedback);
+                            feedback.FireUpdate();
+                            feedback.OutputChange +=
+                                (sender, args) =>
+                                    Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.IntValue);
+
+                            break;
+                        }
                     case JTokenType.String:
-                    {
-                        Debug.Console(2, this, "I AM STRING");
-
-                        trilist.SetStringSigAction(@join, x => WithLock(() =>
                         {
-                            data.SelectToken(path).Replace(x);
-                            saveTimer.Reset(1000);
-                        }));
+                            //Debug.Console(2, this, "I AM STRING");
 
-                        var feedback = new StringFeedback(() => data.SelectToken(path).Value<string>());
-                        feedback.LinkInputSig(trilist.StringInput[@join]);
-                        feedbacks.Add(feedback);
-                        feedback.FireUpdate();
-                        feedback.OutputChange +=
-                            (sender, args) =>
-                                Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.StringValue);
-                    }
-                        break;
+                            trilist.SetStringSigAction(@join, x => WithLock(() =>
+                            {
+                                data.SelectToken(path).Replace(x);
+                                saveTimer.Reset(1000);
+                            }));
+
+                            var feedback = new StringFeedback(() => data.SelectToken(path).Value<string>());
+                            feedback.LinkInputSig(trilist.StringInput[@join]);
+                            feedbacks.Add(feedback);
+                            feedback.FireUpdate();
+                            feedback.OutputChange +=
+                                (sender, args) =>
+                                    Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.StringValue);
+
+                            break;
+                        }
                     case JTokenType.Object:
-                    {
-                        Debug.Console(2, this, "I AM OBJECT");
-
-                        trilist.SetStringSigAction(@join, x =>
                         {
-                            data.SelectToken(path).Replace(x);
-                            saveTimer.Reset(1000);
-                        });
+                            //Debug.Console(2, this, "I AM OBJECT");
 
-                        var feedback = new StringFeedback(() => data.SelectToken(path).Value<string>());
-                        feedback.LinkInputSig(trilist.StringInput[@join]);
-                        feedbacks.Add(feedback);
-                        feedback.FireUpdate();
-                        feedback.OutputChange +=
-                            (sender, args) =>
-                                Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.StringValue);
-                    }
-                        break;
+                            trilist.SetStringSigAction(@join, x =>
+                            {
+                                data.SelectToken(path).Replace(x);
+                                saveTimer.Reset(1000);
+                            });
+
+                            var feedback = new StringFeedback(() => data.SelectToken(path).Value<string>());
+                            feedback.LinkInputSig(trilist.StringInput[@join]);
+                            feedbacks.Add(feedback);
+                            feedback.FireUpdate();
+                            feedback.OutputChange +=
+                                (sender, args) =>
+                                    Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.StringValue);
+
+                            break;
+                        }
                     case JTokenType.Boolean:
-                    {
-                        Debug.Console(2, this, "I AM BOOL");
-
-                        trilist.SetBoolSigAction(@join, x =>
                         {
-                            data.SelectToken(path).Replace(x);
-                            saveTimer.Reset(1000);
-                        });
+                            //Debug.Console(2, this, "I AM BOOL");
 
-                        var feedback = new BoolFeedback(() => data.SelectToken(path).Value<bool>());
-                        feedback.LinkInputSig(trilist.BooleanInput[@join]);
-                        feedbacks.Add(feedback);
-                        feedback.FireUpdate();
-                        feedback.OutputChange +=
-                            (sender, args) =>
-                                Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.BoolValue);
-                    }
-                        break;
+                            trilist.SetBoolSigAction(@join, x =>
+                            {
+                                data.SelectToken(path).Replace(x);
+                                saveTimer.Reset(1000);
+                            });
+
+                            var feedback = new BoolFeedback(() => data.SelectToken(path).Value<bool>());
+                            feedback.LinkInputSig(trilist.BooleanInput[@join]);
+                            feedbacks.Add(feedback);
+                            feedback.FireUpdate();
+                            feedback.OutputChange +=
+                                (sender, args) =>
+                                    Debug.Console(1, this, "Value for path:{0} updated to {1}", path, args.BoolValue);
+
+                            break;
+                        }
+                    default:
+                        {
+                            Debug.Console(0, this, "Cannot map path: '{0}', unsupported type: {1}", path, value.Type);
+                            continue;
+                        }
                 }
             }
         }
